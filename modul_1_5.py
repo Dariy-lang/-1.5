93d669cff37f20bc9d53a61351d33152f37576ac
Dariy-lang <sverhi999@mail.ru>
2024-09-21 00:35:04 +0700

    Add files via upload

---
+++ modul_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = ('cat', 'dog', 2, 5 , 7 )
+print(immutable_var)
+#immutable_var[0] = 'pen'
+#print(immutable_var)
+#Кортеж - это неизменяемый объект, если он будет изменяться, то будет выполнять функцию Списков
+mutable_list = ['red', 'orange', 'green', 'blue']
+print(mutable_list)
+mutable_list [0] = "yellow"
+print(mutable_list)
